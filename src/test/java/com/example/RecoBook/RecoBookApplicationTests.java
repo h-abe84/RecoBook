fab652fbd50d737783cfa9a5d8d44ead91ea845f
abe <hiroto.abe984@gmail.com>
2024-03-25 15:02:41 +0900

    first commit

---
+++ src/test/java/com/example/RecoBook/RecoBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecoBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
